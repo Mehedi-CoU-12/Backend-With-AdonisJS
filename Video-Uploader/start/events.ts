08a1772933bdc01f3a9a8606864164214f029f3e
mehedi_cou_12 <mehedihasana383@gmail.com>
2025-09-15 19:54:10 +0600

    make the console log colorful

--- Video-Uploader/start/events.ts
+++ Video-Uploader/start/events.ts
@@ -1,6 +1,7 @@
 import Event from '@ioc:Adonis/Core/Event'
+import { inspect } from 'util'
 
 Event.on('db:query', function ({ sql, bindings }) {
-  console.log('sql query---->',sql)
-  console.log('bindings----->',bindings)
+  console.log('sql query---->',inspect(sql,{ depth: null, colors: true }))
+  console.log('bindings----->',inspect(bindings,{ depth: null, colors: true }))
 })
